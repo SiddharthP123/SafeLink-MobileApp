daf28c6de4ae62e2daa59973774297595ea8d4d8
Siddharth Premanand <86939549+SiddharthP123@users.noreply.github.com>
2026-07-28 23:34:36 +0530

    Delete services directory

--- services/location.js
+++
@@ -1,8 +0,0 @@
-import * as Location from 'expo-location';
-
-export async function getCurrentLocation() {
-  const { status } = await Location.requestForegroundPermissionsAsync();
-  if (status !== 'granted') throw new Error('Location permission denied');
-  const pos = await Location.getCurrentPositionAsync({});
-  return { lat: pos.coords.latitude, lng: pos.coords.longitude };
-}
